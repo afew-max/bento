42488302e8e5dcd46e795c5eb3bad6d6193a4b4d
afew-max <max@afew-store.com>
2025-03-25 09:54:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,29 +11,29 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	//name: 'John',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
 	// Greetings
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+	//greetingMorning: 'Good morning!',
+	//greetingAfternoon: 'Good afternoon,',
+	//greetingEvening: 'Good evening,',
+	//greetingNight: 'Go to Sleep!',
 
 	// Layout
-	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-	weatherUnit: 'C', // 'F', 'C'
-	language: 'en', // More languages in https://openweathermap.org/current#multi
+	//weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	//weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	//weatherUnit: 'C', // 'F', 'C'
+	//language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	//trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	//defaultLatitude: '37.775',
+	//defaultLongitude: '-122.419',
 
 	// Autochange
 	autoChangeTheme: true,
